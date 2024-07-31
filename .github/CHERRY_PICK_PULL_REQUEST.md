46b7d7d2de00525d107c1aea4dc1fe4702b8cef9
Yosmangel Adames <y.adamespinero@celonis.com>
2024-07-31 19:30:49 +0200

    test new elements

---
+++ .github/CHERRY_PICK_PULL_REQUEST.md
@@ -0,0 +1,13 @@
+# Cherry-Pick PR
+
+**Original PR:** [${{ inputs.branchPr }}](https://github.com/your-repo/pull/${{ inputs.branchPr }})
+
+**Release Type:** ${{ inputs.releaseType }}
+
+**Release Name:** ${{ inputs.releaseName }}
+
+**Root Cause PR:** [${{ inputs.rootCausePr }}](https://github.com/your-repo/pull/${{ inputs.rootCausePr }})
+
+**Jira Ticket:** ${{ inputs.jiraTicket }}
+
+**Reason for Fix:** ${{ inputs.reasonFix }}
--- tools/scripts/github/cherry-pick-creation.mjs
+++ tools/scripts/github/cherry-pick-creation.mjs
@@ -14,11 +14,9 @@ export const cherryPick = async (branchPr, releaseType, releaseName, rootCausePr
         console.log('rootCausePr', rootCausePr);
         console.log('jiraTicket,', jiraTicket);
         console.log('reasonFix', reasonFix);
+
         const prJson = JSON.parse(execSync(`gh pr view ${branchPr} --json author,mergeCommit,mergeable,mergedAt,number,title,url,id,labels`).toString());
         const rootCauseJson = JSON.parse(execSync(`gh pr view ${rootCausePr} --json author,mergeCommit,mergeable,mergedAt,number,title,url,id,labels`).toString());
-
-        console.log('PR_JSON:', prJson.id);
-        console.log('ROOT_CAUSE_JSON:', rootCauseJson.id);
 
         if (prJson.id === rootCauseJson.id) {
             handleError('The PR and the root cause have to be different');
@@ -32,9 +30,7 @@ export const cherryPick = async (branchPr, releaseType, releaseName, rootCausePr
 
         execSync('git fetch --all');
         const branchList = execSync('git branch -r --list "*release*" --format "%(refname:lstrip=3)"').toString();
-        console.log('branchList:' , branchList);
-        console.log('branchList.includes(`release/${releaseName}`):' , branchList.includes(`release/${releaseName}`));
-        console.log('releaseName:' , releaseName);
+
         if (!branchList.includes(`release/${releaseName}`)) {
             handleError("The branch doesn't exist, please make sure that you are using the correct name. Remember to only specify the name of the branch, for example release/BRANCH-NAME**");
         }
@@ -44,6 +40,37 @@ export const cherryPick = async (branchPr, releaseType, releaseName, rootCausePr
             console.log('Emergency fix');
         }
 
+        // Checkout the release branch
+        execSync(`git checkout release/${releaseName}`);
+        const newBranchName = `hotfix/${prJson.author.login}/cherry-pick-automation-${Date.now()}-${prJson.id}`;
+        execSync(`git checkout -b ${newBranchName}`);
+        execSync(`git cherry-pick ${prJson.mergeCommit.oid}`);
+
+        console.log(`Cherry-picked commit ${mergeCommitSha} onto ${newBranchName}`);
+
+        // Push the new branch to remote
+        execSync(`git push origin ${newBranchName}`);
+        const template = fs.readFileSync('CHERRY_PICK_PULL_REQUEST.md', 'utf8');
+        const replacements = {
+            'inputs.branchPr': branchPr,
+            'inputs.releaseType': releaseType,
+            'inputs.releaseName': releaseName,
+            'inputs.rootCausePr': rootCausePr,
+            'inputs.jiraTicket': jiraTicket,
+            'inputs.reasonFix': reasonFix
+        };
+        const prBody = replacePlaceholders(template, replacements);
+        // Write the updated markdown to a temp file
+        const tempFilePath = '/tmp/CHERRY_PICK_PULL_REQUEST.md';
+        fs.writeFileSync(tempFilePath, prBody);
+
+        // Create a new PR
+        const prTitle = `Cherry-pick PR ${branchPr} into release/${releaseName}`;
+        const newPrJson = JSON.parse(execSync(`gh pr create -B release/${releaseName} -H ${newBranchName} -t "${prTitle}" -F ${tempFilePath} --json number,url`).toString());
+
+        console.log(`Created new PR #${newPrJson.number}: ${newPrJson.url}`);
+
+        // Verify release type
         return true;
     } catch (error) {
         handleError(error.message);
